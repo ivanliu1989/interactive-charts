09aa628d3feee3fab55a645e005285f032e2273a
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-08-18 22:03:23 +1000

    shiny

--- App-1/ui.R
+++ App-1/ui.R
@@ -1,24 +1,23 @@
 library(shiny)
 
-# Define UI for application that draws a histogram
+# ui.R
+
 shinyUI(fluidPage(
-    
-    # Application title
-    titlePanel("Hello World!"),
-    
-    # Sidebar with a slider input for the number of bins
+    titlePanel("My Shiny App"),
     sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 5,
-                        max = 50,
-                        value = 30)
-        ),
-        
-        # Show a plot of the generated distribution
+        sidebarPanel(),
         mainPanel(
-            plotOutput("distPlot")
+            p("p creates a paragraph of text. Note: this paragraph is followed by br(), which makes a blank line."),
+            p("A new p() command starts a new paragraph. Supply a style attribute to change the format of the entire paragraph", style = "font-family: 'times'; font-si16pt"),
+            strong("strong() makes bold text."),
+            em("em() creates italicized (i.e, emphasized) text."),
+            br(),
+            code("code displays your text similar to computer code"),
+            div("div creates segments of text with a similar style. This division of text is all blue because I passed the argument 'style = color:blue' to div", style = "color:blue"),
+            br(),
+            p("span does the same thing as div, but it works with",
+              span("groups of words", style = "color:blue"),
+              "that appear inside a paragraph.")
         )
     )
 ))
